ea4ded92748a0c0194a82dd01d5be760f5ca37aa
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2022-05-17 16:56:13 +0530

    [Add] New module Remove Salesperson from Quotation

---
+++ pg_remove_sales_person_from_quotation_print/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name' : 'Remove Salesperson from Quotation Print',
+    'version' : '1.0.0',
+    'summary': 'Removes the Salesperson field from Quotation and Sales Order',
+    'sequence': 31,
+    'category': 'Sales',
+    'description': """
+            If you dont want the Salesperson field to appear on the Print of Quotation and Sales Order, then this module is for you. 
+    """,
+    'website': 'https://www.pushakar.com',
+    'depends' : ['base','mail','sale','sale_management'],
+    'data': ['views/pg_remove_sales_person_from_quotation_print.xml'],
+    'author': 'Pushakar Gaikwad',
+    'license':'OPL-1',
+    'price': 9.99,
+    'currency': 'USD',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
