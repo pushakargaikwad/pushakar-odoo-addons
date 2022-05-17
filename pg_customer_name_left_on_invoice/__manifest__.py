93616153bfcbe0f02251790f6648199899ec1d00
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2022-05-17 16:05:35 +0530

    initial

---
+++ pg_customer_name_left_on_invoice/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name' : 'Customer Name left side on invoice',
+    'version' : '1.0.1',
+    'summary': 'Displays Customer Name on the left side of invoice print',
+    'sequence': 30,
+    "category": "Accounting",
+    'description': """
+            This module displays the Customer Name, Address and other details on the left side of Invoice print
+    """,
+    'website': 'http://www.pushakar.com',
+    'depends' : ['base','account','account_invoicing','web'],
+    'data': ['views/pg_customer_name_left_on_invoice.xml',],
+    'author': 'Pushakar Gaikwad',
+    'license':'OPL-1',
+    'price': 9.99,
+    'currency': 'USD',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
