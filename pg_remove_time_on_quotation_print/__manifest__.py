24ad4394283645c8e90d3b6aeafc6fadcedf46ac
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2022-05-17 16:43:50 +0530

    [Add] New module Remove time from Quotation Print

---
+++ pg_remove_time_on_quotation_print/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name' : 'Remove time on quotation print',
+    'version' : '1.0.0',
+    'summary': 'Removes time from datetime fields of Quotation and Sales Order Print',
+    'sequence': 31,
+    'category': 'Sales',
+    'description': """
+      Sometimes you just want to display date (and not exact time) on Quotation. This is particularly required for fields like Confirmation Date, Order Date and Validity date. This module removes the time information of these fields from the Quotation and Sales Order Print Documents 
+            
+    """,
+    'website': 'https://www.pushakar.com',
+    'depends' : ['base','mail','sale','sale_management'],
+    'data': ['views/pg_remove_time_on_quotation_print.xml'],
+    'author': 'Pushakar Gaikwad',
+    'license':'OPL-1',
+    'price': 9.99,
+    'currency': 'USD',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
