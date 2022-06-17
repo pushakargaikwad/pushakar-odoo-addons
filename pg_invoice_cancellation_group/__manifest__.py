b668e3cbdbeb357f6cb8c71cb3d76f32f7081a3e
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2022-06-17 16:09:31 +0530

    [Add] New Module: Invoice Cancellation Group

---
+++ pg_invoice_cancellation_group/__manifest__.py
@@ -0,0 +1,31 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : 'Invoice Cancellation Group',
+    'version' : '1.0.0',
+    'summary': 'New group for "Cancel Invoice" button',
+    'sequence': 30,
+    'description': """
+            Invoice Cancellation Group
+
+            1. Created new group named as "Invoice Cancel Rights" Under "Allow to cancel invoice" group category.
+            2. This group basically assigned to user who has an authority to cancel the invoice. 
+            3. "Cancel Invoice" button will be visible to this group only. 
+            "Account Cancel" which is a free module is required to be installed and configured for this module to work. 
+            After Installation, To view cancel buttons Invoice and Payment the user should be assigned to the group "Invoice Cancel Rights"
+            Be careful with this module as it has audit implications. Cancelling accounting entries is not authorized in all countries.
+            As "admin" has highest access rights, admin user still has direct access to cancel button without explicitly assigning this group.
+
+    """,
+    'website': 'https://www.pushakar.com',
+    'depends' : ['base','account','account_cancel'],
+    'data': ['views/pg_invoice_cancellation.xml','views/pg_invoice_cancellation_group.xml'],
+    'demo': [],
+    'qweb': [ ],
+    'author': 'Pushakar Gaikwad',
+    'license':'AGPL-3',
+    'price': 19.99,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
