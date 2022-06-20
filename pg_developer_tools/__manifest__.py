90f355b9fd58c38452a03f98f712b8117cb8c625
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2022-06-20 20:10:14 +0530

    [Add] dev tools module [WIP 🚧]

---
+++ pg_developer_tools/__manifest__.py
@@ -0,0 +1,23 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : 'Development Tweaks and Tools to make life Easier',
+    'version' : '1.0.0',
+    'summary': '',
+    'sequence': 31,
+    'description': """
+		.
+            
+    """,
+    'website': 'https://pushakar.com/odoo-development/',
+    'depends' : ['base'],
+    'data': ['views/pg_developer_tools_view.xml'],
+    'demo': [],
+    'qweb': [ ],
+    'author': 'Pushakar Gaikwad',
+    'license':'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
