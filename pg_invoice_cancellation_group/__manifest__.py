de5386c503a75a237a250509fe7adf62c024c4d3
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2022-07-12 15:30:31 +0530

    Updated manifest

--- pg_invoice_cancellation_group/__manifest__.py
+++ pg_invoice_cancellation_group/__manifest__.py
@@ -5,6 +5,7 @@
     'version' : '1.0.0',
     'summary': 'New group for "Cancel Invoice" button',
     'sequence': 30,
+    'category': 'Accounting/Accounting',
     'description': """
             Invoice Cancellation Group
 
@@ -13,12 +14,12 @@
             3. "Cancel Invoice" button will be visible to this group only. 
             
             "Account Cancel" which is a free module is required to be installed and configured for this module to work. 
-            After Installation, To view cancel buttons Invoice and Payment the user should be assigned to the group "Invoice Cancel Rights"
+            After Installation, To view cancel buttons on Invoice and Payment, the user should be assigned to the group "Invoice Cancel Rights"
             Be careful with this module as it has audit implications. Cancelling accounting entries is not authorized in all countries.
             As "admin" has highest access rights, admin user still has direct access to cancel button without explicitly assigning this group.
 
     """,
-    'website': 'https://www.pushakar.com',
+    'website': 'https://pushakar.com/odoo-development/',
     'depends' : ['base','account','account_cancel'],
     'data': ['views/pg_invoice_cancellation.xml','views/pg_invoice_cancellation_group.xml'],
     'demo': [],
