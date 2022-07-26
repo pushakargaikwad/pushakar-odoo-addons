83dea552e32a85dd0a6c19f7997d62b235191669
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2022-07-26 14:20:05 +0530

    [IMP] Display Bank Details Header conditionally

--- pg_bank/__manifest__.py
+++ pg_bank/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name' : 'Bank Module Improvements',
-    'version' : '1.1.1',
+    'version' : '1.2.1',
     'summary': 'Bank Module Improvements',
     'sequence': 30,
     'description': """
